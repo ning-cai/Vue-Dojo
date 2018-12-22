bfadf4b777e9db2b3c564f241bb2adbe6221245b
ning-cai <ning.cai@gmx.de>
2018-12-22 09:22:43 +0100

    Feature: loop onClick event examples

--- helloVue.js
+++ helloVue.js
@@ -11,3 +11,36 @@ const hover = new Vue({
     label: "current date is " + new Date().toLocaleDateString()
   }
 });
+
+const appIf = new Vue({
+  el: "#app-if",
+  data: {
+    display: true
+  }
+});
+
+const appFor = new Vue({
+  el: "#app-for",
+  data: {
+    todos: [
+      { text: "Finish Introduction of Vue.js" },
+      { text: "Read Chapter 14 of a mind for Numbers" },
+      { text: "Check Emails" }
+    ]
+  }
+});
+
+const appOn = new Vue({
+  el: "#app-on",
+  data: {
+    message: "Hello Vue.js!"
+  },
+  methods: {
+    reverseMessage: function() {
+      this.message = this.message
+        .split(" ")
+        .reverse()
+        .join(" ");
+    }
+  }
+});
